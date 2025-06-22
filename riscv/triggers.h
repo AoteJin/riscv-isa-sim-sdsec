4eb1c5163970ec9ba601c4345327bdd8892597f5
Joe Xie <joxie@nvidia.com>
2025-06-22 13:30:38 +0800

    Implement SDSEC sdedbgalw checks in processor step and trigger firing logic. Add can_fire method to manage trigger behavior based on sdedbgalw state in supervisor mode.

--- riscv/triggers.h
+++ riscv/triggers.h
@@ -99,6 +99,7 @@ public:
 protected:
   static action_t legalize_action(reg_t val, reg_t action_mask, reg_t dmode_mask) noexcept;
   bool common_match(processor_t * const proc, bool use_prev_prv = false) const noexcept;
+  bool can_fire(processor_t * const proc, bool use_prev_prv = false) const noexcept;
   reg_t tdata2;
 
   bool vs = false;
