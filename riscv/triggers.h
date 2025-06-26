f1828cf1cc3d2a89369131346dc445ed32d749b4
aotej <aotej@nvidia.com>
2025-06-26 23:32:33 +0800

    - Unifiy and refine the method to check whether debug is allowed - Update dmode and dmprv and ebreak behavior - Update debug rom

--- riscv/triggers.h
+++ riscv/triggers.h
@@ -99,7 +99,6 @@ public:
 protected:
   static action_t legalize_action(reg_t val, reg_t action_mask, reg_t dmode_mask) noexcept;
   bool common_match(processor_t * const proc, bool use_prev_prv = false) const noexcept;
-  bool can_fire(processor_t * const proc, bool use_prev_prv = false) const noexcept;
   reg_t tdata2;
 
   bool vs = false;
