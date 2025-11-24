2f9c4d4f1dec572ce0a673e12c2096fe7f9189d0
Andrew Waterman <andrew@sifive.com>
2025-11-23 19:38:25 -0800

    Rebuild opcode map whenever ISA/logging changes
    This facilitates caching less info in the opcode cache.

--- riscv/processor.h
+++ riscv/processor.h
@@ -350,6 +350,7 @@ public:
     register_insn(insn, true /* is_custom */);
   }
   void register_extension(extension_t*);
+  void build_opcode_map();
 
   // MMIO slave interface
   bool load(reg_t addr, size_t len, uint8_t* bytes) override;
@@ -436,7 +437,6 @@ private:
   friend class extension_t;
 
   void parse_priv_string(const char*);
-  void build_opcode_map();
   void register_base_instructions();
   insn_func_t decode_insn(insn_t insn);
 
