riscv-software-src/riscv-isa-sim

Add Zvfbfa, Zvfofp8min, Zvfofp4min support

riscv/insns/vfmacc_vf.h
@@ -1,7 +1,10 @@
 // vfmacc.vf vd, rs1, vs2, vm    # vd[i] = +(vs2[i] * x[rs1]) + vd[i]
+
+require_zvfbfa
+
 VI_VFP_VF_LOOP
 ({
-  vd = f16_mulAdd(rs1, vs2, vd);
+  vd = VFP_MULLADD_16(rs1, vs2, vd);
 },
 {
   vd = f32_mulAdd(rs1, vs2, vd);

riscv/insns/vfmacc_vv.h
@@ -1,7 +1,10 @@
 // vfmacc.vv vd, rs1, vs2, vm    # vd[i] = +(vs2[i] * vs1[i]) + vd[i]
+
+require_zvfbfa
+
 VI_VFP_VV_LOOP
 ({
-  vd = f16_mulAdd(vs1, vs2, vd);
+  vd = VFP_MULLADD_16(vs1, vs2, vd);
 },
 {
   vd = f32_mulAdd(vs1, vs2, vd);

AoteJin/riscv-isa-sim-sdsec

riscv/insns/vfmacc_vf.h
@@ -1,7 +1,10 @@
 // vfmacc.vf vd, rs1, vs2, vm    # vd[i] = +(vs2[i] * x[rs1]) + vd[i]
+
+require_zvfbfa
+
 VI_VFP_VF_LOOP
 ({
-  vd = f16_mulAdd(rs1, vs2, vd);
+  vd = VFP_MULADD_16(rs1, vs2, vd);
 },
 {
   vd = f32_mulAdd(rs1, vs2, vd);

riscv/insns/vfmacc_vv.h
@@ -1,7 +1,10 @@
 // vfmacc.vv vd, rs1, vs2, vm    # vd[i] = +(vs2[i] * vs1[i]) + vd[i]
+
+require_zvfbfa
+
 VI_VFP_VV_LOOP
 ({
-  vd = f16_mulAdd(vs1, vs2, vd);
+  vd = VFP_MULADD_16(vs1, vs2, vd);
 },
 {
   vd = f32_mulAdd(vs1, vs2, vd);